e220e78b5441e02d25edfd17ecef88bbe9ff05fa
daoud <daoud@ensta.fr>
2016-11-16 11:16:02 +0100

    Isometric module

--- js/displayGame.js
+++ js/displayGame.js
@@ -1,12 +1,27 @@
 /* global Graphics, Sprite, engine */
 
-const isometricTiles = { dx: 64, dy: 32, dz: 16 }
+const Isometric = {
+  tiles: { dx: 64, dy: 32, dz: 16 },
+  pos (wx, wy) {
+    return [
+      (wx - wy) * Isometric.tiles.dx / 2,
+      -(wy + wx) * Isometric.tiles.dy / 2,
+      -(wy + wx)
+    ]
+  },
+  updateSize () {
+    this.tiles.width = this.tiles.dx
+    this.tiles.height = this.tiles.dy + this.tiles.dz
+  }
+}
+Isometric.updateSize()
+
 const obstacleHeight = 96
 
 function isometricPos (wx, wy) {
   return [
-    (wx - wy) * isometricTiles.dx / 2,
-    -(wy + wx) * isometricTiles.dy / 2,
+    (wx - wy) * Isometric.tiles.dx / 2,
+    -(wy + wx) * Isometric.tiles.dy / 2,
     -(wy + wx)
   ]
 }
@@ -14,22 +29,23 @@ function isometricPos (wx, wy) {
 function displayGame () {
   // Set origin
   Graphics.ox = Graphics.width / 2
-  Graphics.oy = Graphics.height - isometricTiles.dz * 2
+  Graphics.oy = Graphics.height - Isometric.tiles.dz * 2
 
   // Set scaling
-  if (engine.mapSize * isometricTiles.dx > Graphics.width * 0.9) {
-    Graphics.scale = (Graphics.width * 0.9) / (engine.mapSize * isometricTiles.dx)
+  if (engine.mapSize * Isometric.tiles.dx > Graphics.width * 0.9) {
+    Graphics.scale = (Graphics.width * 0.9) / (engine.mapSize * Isometric.tiles.dx)
   }
 
-  let tileW = isometricTiles.dx
-  let tileH = isometricTiles.dy + isometricTiles.dz
+  // Render
+  const tileW = Isometric.tiles.width
+  const tileH = Isometric.tiles.height
   for (let wx = 0; wx < engine.mapSize; wx++) {
     for (let wy = 0; wy < engine.mapSize; wy++) {
       // Terrain
       let terrainType = engine.terrainMap[wx][wy]
       let tile = new Sprite('terrain_tiles.png', terrainType * tileW, 0, tileW, tileH)
       tile.setPosition(...isometricPos(wx, wy))
-      tile.setOrigin(isometricTiles.dx / 2, isometricTiles.dy)
+      tile.setOrigin(Isometric.tiles.dx / 2, Isometric.tiles.dy)
       Graphics.add(tile)
 
       // Obstacles
@@ -37,7 +53,7 @@ function displayGame () {
       if (obstacleType) {
         let obstacle = new Sprite('obstacle_tiles.png', obstacleType * tileW, 0, tileW, obstacleHeight)
         obstacle.setPosition(...isometricPos(wx, wy))
-        obstacle.setOrigin(isometricTiles.dx / 2, obstacleHeight)
+        obstacle.setOrigin(Isometric.tiles.dx / 2, obstacleHeight)
         obstacle.z += 1e3
         Graphics.add(obstacle)
       }
