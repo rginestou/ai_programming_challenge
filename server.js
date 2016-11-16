3bf46cf424f165177c7624ba724f1adfa469170b
daoud <daoud@ensta.fr>
2016-11-16 14:47:56 +0100

    use socket.io

--- server.js
+++ server.js
@@ -3,5 +3,15 @@ const config = require('./config')
 const express = require('express')
 const app = express()
 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' })
+  socket.on('my other event', function (data) {
+    console.log(data)
+  })
+})
+
 app.use(express.static('public'))
-app.listen(config.port, () => console.log('App running on port ' + config.port))
+server.listen(config.port, () => console.log('App running on port ' + config.port))
