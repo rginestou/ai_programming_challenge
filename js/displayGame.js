023bf12251c2290dc723bdd26d1df8a6ea41eaab
daoud <daoud@ensta.fr>
2016-11-16 10:48:07 +0100

    Isometric module

--- js/displayGame.js
+++ js/displayGame.js
@@ -1,34 +1,36 @@
 /* global Graphics, Sprite, engine */
 
-const isometricTiles = { dx: 64, dy: 32, dz: 16 }
-const tileW = isometricTiles.dx
-const tileH = isometricTiles.dy + isometricTiles.dz
-
-function isometricPos (wx, wy) {
-  return [
-    (wx - wy) * isometricTiles.dx / 2,
-    -(wy + wx) * isometricTiles.dy / 2,
-    -(wy + wx)
-  ]
+const Isometric = {
+  tiles: { dx: 64, dy: 32, dz: 16 },
+  position (wx, wy) {
+    return [
+      (wx - wy) * this.tiles.dx / 2,
+      -(wy + wx) * this.tiles.dy / 2,
+      -(wy + wx)
+    ]
+  }
 }
+
+Isometric.tiles.width = Isometric.tiles.dx
+Isometric.tiles.height = Isometric.tiles.dy + Isometric.tiles.dz
 
 function displayGame () {
   // Set origin
   Graphics.ox = Graphics.width / 2
-  Graphics.oy = Graphics.height - isometricTiles.dz * 2
+  Graphics.oy = Graphics.height - Isometric.tiles.dz * 2
 
   // Set scaling
-  if (engine.mapSize * isometricTiles.dx > Graphics.width * 0.9) {
-    Graphics.scale = (Graphics.width * 0.9) / (engine.mapSize * isometricTiles.dx)
+  if (engine.mapSize * Isometric.tiles.dx > Graphics.width * 0.9) {
+    Graphics.scale = (Graphics.width * 0.9) / (engine.mapSize * Isometric.tiles.dx)
   }
 
   // Render terrain
   for (let wx = 0; wx < engine.mapSize; wx++) {
     for (let wy = 0; wy < engine.mapSize; wy++) {
       let terrainType = engine.terrainMap[wx][wy]
-      let tile = new Sprite('terrain_tiles.png', terrainType * tileW, 0, tileW, tileH)
-      tile.setPosition(...isometricPos(wx, wy))
-      tile.setOrigin(isometricTiles.dx / 2, isometricTiles.dy)
+      let tile = new Sprite('terrain_tiles.png', terrainType * Isometric.tiles.width, 0, Isometric.tiles.width, Isometric.tiles.height)
+      tile.setPosition(...Isometric.position(wx, wy))
+      tile.setOrigin(Isometric.tiles.dx / 2, Isometric.tiles.dy)
       Graphics.add(tile)
     }
   }
--- js/gameObjects.js
+++ js/gameObjects.js
@@ -1,4 +1,4 @@
-/* global isometricPos, Sprite, tileW, isometricTiles */
+/* global Isometric, Sprite */
 
 class gameObjects {
   constructor (id, x, y) {
@@ -12,7 +12,7 @@ class gameObjects {
 
   setupSprite (src, x, y, w, h, ox, oy) {
     this.sprite = new Sprite(src, x, y, w, h)
-    this.sprite.setPosition(...isometricPos(this.x, this.y))
+    this.sprite.setPosition(...Isometric.position(this.x, this.y))
     this.sprite.setOrigin(ox, oy)
     this.sprite.z += 1e3
   }
@@ -24,7 +24,9 @@ class Obstacle extends gameObjects {
   }
 
   setupSprite () {
-    super.setupSprite('obstacle_tiles.png', this.id * tileW, 0, tileW, 96, isometricTiles.dx / 2, 96)
+    let h = 96
+    let w = Isometric.tiles.width
+    super.setupSprite('obstacle_tiles.png', this.id * w, 0, w, h, w / 2, h)
   }
 }
 
@@ -36,7 +38,7 @@ class Building extends gameObjects {
 
   setupSprite () {
     let h = 96
-    let w = this.size * tileW
+    let w = this.size * Isometric.tiles.width
     super.setupSprite('buildings.png', (this.id - 1) * w, (this.size - 1) * h, w, h, w / 2, h)
   }
 }
