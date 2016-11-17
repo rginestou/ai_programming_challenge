26bd733d3a6125c408668afbb9f02a8ef58161fa
daoud <daoud@ensta.fr>
2016-11-18 00:56:57 +0100

    map generator

--- server.js
+++ server.js
@@ -17,19 +17,22 @@ app.use(express.static('public'))
 server.listen(config.server.port, () => console.log('App running on port ' + config.server.port))
 
 // ===
-const GameTeam = require('./game/team')
-const GameUnit = require('./game/unit')
-const GameObstacle = require('./game/obstacle')
-const GameBuilding = require('./game/building')
+// const GameTeam = require('./game/team')
+// const GameUnit = require('./game/unit')
+// const GameObstacle = require('./game/obstacle')
+// const GameBuilding = require('./game/building')
+//
+// let team = new GameTeam(1)
+//
+// /* eslint-disable no-new */
+// new GameObstacle(0, 4, 6)
+//
+// new GameUnit(team, 1, 2, 3)
+// new GameUnit(team, 1, 3, 3)
+// new GameUnit(team, 1, 4, 3)
+// new GameBuilding(team, 2, 2, 2)
+//
+// console.log(team.jsonify())
 
-let team = new GameTeam(1)
-
-/* eslint-disable no-new */
-new GameObstacle(0, 4, 6)
-
-new GameUnit(team, 1, 2, 3)
-new GameUnit(team, 1, 3, 3)
-new GameUnit(team, 1, 4, 3)
-new GameBuilding(team, 2, 2, 2)
-
-console.log(team.jsonify())
+const generate = require('./utils/mapGenerator')
+console.log(generate({ mapSize: 10 }))
